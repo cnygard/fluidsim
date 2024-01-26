15422aed72ee97acbee9f08c5542ee93fb2a823f
Christian Nygard <seajay.nygard@gmail.com>
2024-01-26 12:42:41 -0500

    added gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include "../include/path.hpp"
 
 int main(int argc, char** argv) {
-  Path* path = Path::get(argv[0]);
-  std::cout << path->append("/hi") << std::endl;
+  // Path* path = Path::get(argv[0]);
+  // std::cout << path->append("/hi") << std::endl;
+  std::cout << "hi" << std::endl;
   return 0;
 }
